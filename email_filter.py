5d8c827ca33d30ca046ff4fe1b7ad31753746899
Projesh <projesh.bhoumik07@gmail.com>
2017-11-06 01:26:04 +0600

    Hacker rank problems solve

---
+++ email_filter.py
@@ -0,0 +1,31 @@
+import string
+allowed = string.letters + string.digits + '_' + '-'
+
+def fun(in_val):
+
+	check=all(c in allowed for c in in_val)
+	if check:
+		atindex=in_val.index('@')
+		dotindex=in_val.index('.')
+
+		if atindex<1 or dotindex<atindex+1 or dotindex+3>=len(in_val):
+			return False
+		else:
+			return True
+	else:
+		return False
+				
+
+
+def filter_mail(emails):
+    return filter(fun, emails)
+
+if __name__ == '__main__':
+    n = int(raw_input())
+    emails = []
+    for _ in range(n):
+        emails.append(raw_input())
+
+    filtered_emails = filter_mail(emails)
+    filtered_emails.sort()
+    print filtered_emails
